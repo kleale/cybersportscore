02e95795cbc223456509fa5c845bccd6866f2ef0
kleale <klealekle@gmail.com>
2023-01-11 11:56:23 +0300

    login fix

--- src/pages/LoginView.vue
+++ src/pages/LoginView.vue
@@ -2,9 +2,10 @@
     <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8 ">
         <div class="w-full max-w-md space-y-8">
             <div>
-                <img class="mx-auto h-12" src="../assets/logo.svg" width="210"
-                    alt="CyberSportScore" />
-                <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-400">Войти в свой профиль
+                <!-- <img class="mx-auto h-12" src="../assets/logo.svg" width="210"
+                    alt="CyberSportScore" /> -->
+                <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-400">Войти в
+                    свой профиль
                 </h2>
                 <!-- <p class="mt-2 text-center text-sm text-gray-600">
                     Or
@@ -19,32 +20,30 @@
                 <div class="-space-y-px rounded-md shadow-sm">
                     <div>
                         <label for="email-address" class="sr-only">Email</label>
-                        <input id="email-address" name="email" type="email" autocomplete="email" required=""
-                            class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 
+                        <input id="email-address" name="email" type="email" autocomplete="email" required="" class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 
                             placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm
-                            dark:text-gray-200 dark:bg-gray-800 dark:border-gray-600"
-                            placeholder="Email" />
+                            dark:text-gray-200 dark:bg-gray-800 dark:border-gray-600" placeholder="Email" />
                     </div>
                     <div>
                         <label for="password" class="sr-only">Пароль</label>
                         <input id="password" name="password" type="password" autocomplete="current-password" required=""
                             class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 
                             placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm
-                            dark:text-gray-200 dark:bg-gray-800 dark:border-gray-600"
-                            placeholder="Пароль" />
+                            dark:text-gray-200 dark:bg-gray-800 dark:border-gray-600" placeholder="Пароль" />
                     </div>
                 </div>
 
                 <div class="flex items-center justify-between">
                     <div class="flex items-center">
-                        <input id="remember-me" name="remember-me" type="checkbox"
-                            class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500
+                        <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500
                             dark:text-gray-200 dark:bg-gray-800 dark:border-gray-600" />
-                        <label for="remember-me" class="ml-2 block text-sm text-gray-900 dark:text-gray-400">Запомнить меня</label>
+                        <label for="remember-me" class="ml-2 block text-sm text-gray-900 dark:text-gray-400">Запомнить
+                            меня</label>
                     </div>
 
                     <div class="text-sm">
-                        <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500 dark:text-gray-400">Я не помню пароль</a>
+                        <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500 dark:text-gray-400">Я не
+                            помню пароль</a>
                     </div>
                 </div>
 
