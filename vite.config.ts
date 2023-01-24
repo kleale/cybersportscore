bc235adbde76d345b5dc56f3203c798bf970b50f
kleale <klealekle@gmail.com>
2023-01-24 11:24:34 +0300

    jquery import

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import inject from "@rollup/plugin-inject";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/cybersportscore/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    inject({   // => that should be first under plugins array
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+  ],
 })
